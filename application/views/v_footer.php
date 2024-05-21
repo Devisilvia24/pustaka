2b7e8a6b22ef7d586f16625abc79b26dca6dbd81
Devisilvia24 <118919254+Devisilvia24@users.noreply.github.com>
2024-05-21 20:27:45 +0700

    first commit

---
+++ application/views/v_footer.php
@@ -0,0 +1,13 @@
+<DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta Charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body>
+    <footer>
+        <a href="http://www.RentalBuku.com">RentalBuku</a>
+        </footer>
+ </body>
+</html>
